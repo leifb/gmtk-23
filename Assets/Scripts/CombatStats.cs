9ddc0b5bfbe60243ee4095a7dc1de39484ff8944
leifb <leif.bernsdorf@gmail.com>
2023-07-08 00:31:42 +0200

    Added basic combat stats

--- Assets/Scripts/CombatStats.cs
+++ Assets/Scripts/CombatStats.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatStats : MonoBehaviour
+/// This class represents the combat stats (damage scaling) of an enemy.
+public class CombatStats
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// Increases blunt damage
+    public float strength = 1.0;
+    /// Increases piercing damage
+    public float dexterity = 1.0;
+    /// Increases intelligence damage
+    public float intelligence = 1.0;
+
+    // Decreases blunt damage
+    public float constitution = 1.0;
+    /// Increases piercing damage
+    public float agility = 1.0;
+    /// Increases intelligence damage
+    public float willpower = 1.0;
+
+
+    /// Is always created with default values
+    public CombatStats() {
+
     }
 }
