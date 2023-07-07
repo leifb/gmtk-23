a35c730908dbb887affe9a436a40d82e9f05bab5
nero97ful <nero97ful@googlemail.com>
2023-07-08 00:18:10 +0200

    scene

--- Assets/abstr.cs
+++ Assets/abstr.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class abstr : MonoBehaviour
 {
-
-    public GameObject enemy;
-    
     // Start is called before the first frame update
     void Start()
     {
