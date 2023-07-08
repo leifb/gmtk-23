186728fd8ceda585cc5535b6648f4527ae695475
nero97ful <nero97ful@googlemail.com>
2023-07-08 14:22:09 +0200

    enemy controller spawning on tiles

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -6,22 +6,29 @@ public class EnemyAi : MonoBehaviour
 {
 
     public double speed = 3.0;
+    public float spawnRadius = 5f;
 
     private Transform target;
 
     private MovementLogic movement;
+    private CircleCollider2D spawnCircleCollider;
 
     // Start is called before the first frame update
     void Start()
     {
         this.target = GameObject.Find("character").transform;
         this.movement = new DefaultMovement(this.speed);
+        this.spawnCircleCollider.radius = spawnRadius;
     }
 
     // Update is called once per frame
     void Update()
     {
         this.movement.Move(this.transform, this.target);
+    }
+
+    public CircleCollider2D GetSpawnCircleCollider(){
+        return spawnCircleCollider;
     }
 
     
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,19 +13,22 @@ public class EnemyController : MonoBehaviour
     public Transform spawnParent;
     private float nextSpawn = 2f;
     private bool spawnHorde = true;
-    private float spawnDistanceRadius = 10f;
     private Vector2Int spawnArea;
     public UnityEngine.Tilemaps.Tilemap background;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < background.size.x; i++){
-            for (int j = 0; j < background.size.y; j++){
-                if (Mathf.Abs(spawnArea.x) > Mathf.Abs(background.CellToWorld(new Vector3Int(i, j, 0)).x)){
+        for (int i = 0; i < background.size.x; i++)
+        {
+            for (int j = 0; j < background.size.y; j++)
+            {
+                if (Mathf.Abs(spawnArea.x) > Mathf.Abs(background.CellToWorld(new Vector3Int(i, j, 0)).x))
+                {
                     spawnArea.x = Mathf.FloorToInt(background.CellToWorld(new Vector3Int(i, j, 0)).x);
                 }
-                if (Mathf.Abs(spawnArea.y) < Mathf.Abs(background.CellToWorld(new Vector3Int(i, j, 0)).y)){
+                if (Mathf.Abs(spawnArea.y) < Mathf.Abs(background.CellToWorld(new Vector3Int(i, j, 0)).y))
+                {
                     spawnArea.y = Mathf.FloorToInt(background.CellToWorld(new Vector3Int(i, j, 0)).y);
                 }
             }
@@ -61,28 +64,35 @@ public class EnemyController : MonoBehaviour
 
     void SpawnEnemy(GameObject enemy)
     {
-        Vector3Int pos = new Vector3Int(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), 0);
-        
-        UnityEngine.Tilemaps.TileBase tile = background.GetTile(pos);
+        EnemyAi enemyAi = enemy.GetComponent<EnemyAi>();
+        CircleCollider2D spawnCircleCollider;
+        if (enemyAi != null)
+        {
+            spawnCircleCollider = enemyAi.GetSpawnCircleCollider();
 
-        while (pos.x == 0 && hero.transform.position.x - spawnDistanceRadius <= pos.x && hero.transform.position.x + spawnDistanceRadius >= pos.x)
-        {
-            pos.x = Random.Range(-spawnArea.x, spawnArea.x);
-        }
-        while (pos.y == 0 && hero.transform.position.y - spawnDistanceRadius <= pos.y && hero.transform.position.y + spawnDistanceRadius >= pos.y)
-        {
-            pos.y = Random.Range(-spawnArea.y, spawnArea.y);
-        }
+            Vector3Int pos = new Vector3Int(Random.Range(-spawnArea.x, spawnArea.x), Random.Range(-spawnArea.y, spawnArea.y), 0);
 
-        if (!spawnHorde)
-        {
-            Instantiate(enemy, pos, Quaternion.identity);
-        }
-        else
-        {
-            for (int i = 0; i < Random.Range(3, 7); i++)
+            UnityEngine.Tilemaps.TileBase tile = background.GetTile(pos);
+
+            while (pos.x == 0 || tile == null || spawnCircleCollider.IsTouching(hero.GetComponent<Collider2D>()))
             {
-                Instantiate(enemy, PositionNearby(pos), Quaternion.identity, spawnParent);
+                pos.x = Random.Range(-spawnArea.x, spawnArea.x);
+            }
+            while (pos.y == 0 || tile == null || spawnCircleCollider.IsTouching(hero.GetComponent<Collider2D>()))
+            {
+                pos.y = Random.Range(-spawnArea.y, spawnArea.y);
+            }
+
+            if (!spawnHorde)
+            {
+                Instantiate(enemy, pos, Quaternion.identity, spawnParent);
+            }
+            else
+            {
+                for (int i = 0; i < Random.Range(3, 7); i++)
+                {
+                    Instantiate(enemy, PositionNearby(pos), Quaternion.identity, spawnParent);
+                }
             }
         }
 
