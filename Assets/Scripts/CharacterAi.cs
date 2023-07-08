b6bdf164e1292d3975366861f176763bd5fe2dd1
leifb <leif.bernsdorf@gmail.com>
2023-07-08 22:43:57 +0200

    remove old movement logic

--- Assets/Scripts/CharacterAi.cs
+++ Assets/Scripts/CharacterAi.cs
@@ -16,31 +16,8 @@ public class CharacterAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.UpdateTarget();
     }
 
-
-    /// Checks if a new target is needed and selects one if necessary
-    /// returns whether a valid target is set.
-    private void UpdateTarget() {
-        if (this.target.isValid()) {
-            return;
-        }
-
-        // Select new target
-        this.target.set(this.SelectTarget());
-    }
-
-    private Transform SelectTarget() {
-        int amountEnemies = this.enemiesParent.childCount;
-
-        if (amountEnemies < 1) {
-            return null;
-        }
-
-        int selectedEnemy = Random.Range(0, amountEnemies);
-        return this.enemiesParent.GetChild(selectedEnemy).transform;
-    }
 
     public void Sleep(double duration)
     {
