cfe32eb98bdc446a738db3645b260af2f4a127f7
leifb <leif.bernsdorf@gmail.com>
2023-07-09 18:10:21 +0200

    Made the music loop

--- Assets/Audio/BackgroundMusic.cs
+++ Assets/Audio/BackgroundMusic.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class BackgroundMusic : MonoBehaviour
 {
     AudioSource audioSource;
+    
+    public AudioClip clipA;
+    public AudioClip clipB;
 
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState((int)System.DateTime.Now.Ticks);
         this.audioSource = this.GetComponent<AudioSource>();
         this.UpdateVolume(0.5f);
+
+        this.audioSource.clip = GetRandonmClip();
+        this.audioSource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.audioSource.isPlaying)
+            return;
+
+        this.PlayNextClip();
+    }
+
+    private void PlayNextClip() {
+        if (this.audioSource.clip == this.clipA) {
+            this.audioSource.clip = this.clipB;
+        }
+        else {
+            this.audioSource.clip = this.clipA;
+        }
+        this.audioSource.Play();
+    }
+
+
+    private AudioClip GetRandonmClip() {
+        if (Random.value < 0.5f)
+            return this.clipA;
+        return this.clipB;
     }
 
     public void UpdateVolume(float value) {
